e6f1f498a085fde9bbe68f118609ec02ce714b15
Saken Meirzhan <meirzhansaken096@gmail.com>
2026-02-17 12:01:08 +0500

    first task

---
+++ Practice1/Problem1.kt
@@ -0,0 +1,26 @@
+package Practice1
+
+fun main() {
+    val myName: String = readln()
+    printMyName(myName)
+}
+
+fun printMyName(myName: String){
+    for (i in 0 until 3){
+        if(i == 0 || i ==2){
+            if (i == 2) {
+                println()
+            }
+            for (j in 0..myName.length + 1){
+                if (j == 0 || j == myName.length + 1){
+                    print("+")
+                }else{
+                    print("-")
+                }
+            }
+        } else{
+            println()
+            print("|$myName|")
+        }
+    }
+}
